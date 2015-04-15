5f4f751e03593512d2696ffb96e1a36b53f77062
Johnny Oshika <johnnyoshika@gmail.com>
2015-04-15 09:42:22 -0700

    filled in collapsible-navbar component content

--- addon/components/collapsible-navbar.js
+++ addon/components/collapsible-navbar.js
@@ -2,5 +2,23 @@ import Ember from 'ember';
 import layout from '../templates/components/collapsible-navbar';
 
 export default Ember.Component.extend({
-  layout: layout
+
+    layout: layout,
+
+    classNames: ['navbar-wrapper'],
+
+    _onNavClick: function() {
+        var toggle = this.$('.navbar-toggle');
+        if (toggle && toggle.is(':visible'))
+            toggle.trigger('click');
+    },
+
+    didInsertElement: function() {
+        this.$('.nav').on('click', 'a', this._onNavClick.bind(this));
+    },
+
+    willDestroyElement: function() {
+        this.$('.nav').off('click', 'a', this._onNavClick.bind(this));
+    }
+
 });
